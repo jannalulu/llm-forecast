56a50a558a1ba33c42b32a3a309f48f4c6e15bff
Shroomy Shroom <sporeventexplosion@gmail.com>
2024-07-08 07:44:49 +0000

    virtualenv setup

--- setup-env.sh
+++ setup-env.sh
@@ -4,4 +4,5 @@ export PYENV_ROOT="$HOME/.pyenv"
 command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"
 eval "$(pyenv init -)"
 
+[ -f venv/bin/activate ] || python3 -m virtualenv venv
 source venv/bin/activate
